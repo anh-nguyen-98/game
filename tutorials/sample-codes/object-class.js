f42ffa609399cd726357f88050e96ba09a8ff385
anh-nguyen-98 <64317700+anh-nguyen-98@users.noreply.github.com>
2022-06-01 02:31:06 +0700

    Update object-class.js

--- tutorials/sample-codes/object-class.js
+++ tutorials/sample-codes/object-class.js
@@ -1,1 +1,56 @@
+// using Object() constructor
+var studentA = new Object();
+studentA.name = "Anh";
 
+// using object literal syntax
+
+var studentB = {
+  name: "Anh",
+  age: 18
+}
+
+// using a user-defined class
+class Person {
+
+  name;
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}`);
+  }
+
+}
+var studentC = new Person("Anh");
+
+class Professor extends Person {
+
+  teaches;
+
+  constructor(name, teaches) {
+    super(name);
+    this.teaches = teaches;
+  }
+
+  introduceSelf() {
+    console.log(`My name is ${this.name}, and I will be your ${this.teaches} professor.`);
+  }
+
+  grade(paper) {
+    const grade = Math.floor(Math.random() * (5 - 1) + 1);
+    console.log(grade);
+  }
+
+}
+
+var prof = new Professor("Lan", "Math");
+prof.grade("190004-PLP");
+
+prof.teaches = "Science";
+
+prof.introduceSelf;
+
+studentC.introduceSelf();
+prof.instroduceSelf();
